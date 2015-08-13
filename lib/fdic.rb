d1d2e02764d136724c7d4d60721380c9bc3298b4
Dan Bernier <dbernier@continuity.net>
2015-08-13 21:16:58 +0000

    Move FDIC::BankFind files into fdic/bank_find folder

--- lib/fdic.rb
+++ lib/fdic.rb
@@ -1,48 +1,4 @@
 require 'httparty'
 require 'logger'
 require "fdic/version"
-require 'fdic/exceptions'
-require 'fdic/client'
-require 'fdic/record'
-require 'fdic/bank'
-require 'fdic/institution'
-require 'fdic/branch'
-require 'fdic/history_event'
-
-module FDIC
-  module BankFind
-
-    def find_bank(bank_name)
-      resp = Client.new.find_bank(bank_name)
-      resp['d']['results'].map { |result|
-        Bank.new(result)
-      }
-    end
-
-    def find_institution(certificate_number)
-      resp = Client.new.find_institution(certificate_number)
-      results = resp.fetch('d').fetch('results')
-      if results.empty? || results.nil?
-        raise FDIC::Exceptions::RecordNotFound, "#{certificate_number} appears to be an invalid certificate number"
-      else
-        Institution.new(results.first)
-      end
-    end
-
-    def find_branches(certificate_number)
-      resp = Client.new.find_branches(certificate_number)
-      resp['d']['results'].map { |result|
-        Branch.new(result)
-      }
-    end
-
-    def find_history_events(bank_name, certificate_number)
-      resp = Client.new.find_history_events(bank_name, certificate_number)
-      resp['d']['results'].map { |result|
-        HistoryEvent.new(result)
-      }
-    end
-
-    extend self
-  end
-end
+require 'fdic/bank_find'
---
+++ lib/fdic/bank_find.rb
@@ -0,0 +1,45 @@
+require 'fdic/bank_find/exceptions'
+require 'fdic/bank_find/client'
+require 'fdic/bank_find/record'
+require 'fdic/bank_find/bank'
+require 'fdic/bank_find/institution'
+require 'fdic/bank_find/branch'
+require 'fdic/bank_find/history_event'
+
+module FDIC
+  module BankFind
+
+    def find_bank(bank_name)
+      resp = Client.new.find_bank(bank_name)
+      resp['d']['results'].map { |result|
+        Bank.new(result)
+      }
+    end
+
+    def find_institution(certificate_number)
+      resp = Client.new.find_institution(certificate_number)
+      results = resp.fetch('d').fetch('results')
+      if results.empty? || results.nil?
+        raise FDIC::Exceptions::RecordNotFound, "#{certificate_number} appears to be an invalid certificate number"
+      else
+        Institution.new(results.first)
+      end
+    end
+
+    def find_branches(certificate_number)
+      resp = Client.new.find_branches(certificate_number)
+      resp['d']['results'].map { |result|
+        Branch.new(result)
+      }
+    end
+
+    def find_history_events(bank_name, certificate_number)
+      resp = Client.new.find_history_events(bank_name, certificate_number)
+      resp['d']['results'].map { |result|
+        HistoryEvent.new(result)
+      }
+    end
+
+    extend self
+  end
+end
