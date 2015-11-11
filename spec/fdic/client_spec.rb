17a4a48ff7f1986605383a249d6e0c1bd06410b2
Tom Reznick <treznick@continuty.net>
2015-11-11 22:57:31 +0000

    check the response code of the FDIC, if the code is in the 500 range, raise a ServerError.

--- spec/fdic/client_spec.rb
+++ spec/fdic/client_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
 describe FDIC::BankFind::Client do
-  it "escapes single-quotes by doubling them up. ' -> ''" do
-    expect(FDIC::BankFind::Client).to receive(:get) do |action, query|
-      expect(query[:query]['$filter']).to include("PEOPLE''S UNITED")
+  describe '#find_bank' do
+    it "escapes single-quotes by doubling them up. ' -> ''" do
+      safe_response = double("HTTParty::Response", code: 200)
+      expect(FDIC::BankFind::Client).to receive(:get) do |action, query|
+        expect(query[:query]['$filter']).to include("PEOPLE''S UNITED")
+      end.and_return(safe_response)
+
+      FDIC::BankFind::Client.new.find_bank("People's United")
     end
 
-    FDIC::BankFind::Client.new.find_bank("People's United")
+    context 'when the FDIC is having struggles' do
+      it 'raises an exception' do
+        response = double("HTTParty::Response", code: 500)
+        allow(FDIC::BankFind::Client).to receive(:get).and_return(response)
+
+        expect {
+          FDIC::BankFind::Client.new.find_bank("foo")
+        }.to raise_error(FDIC::Exceptions::ServerError)
+      end
+    end
   end
 end
